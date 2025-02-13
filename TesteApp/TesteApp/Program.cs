e946be7f44313b5068058392e0bf0023e00fc0b3
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-02-13 13:02:07 +0000

    Added Library solution (LibStudents class) and TesteApp (to try methods)

---
+++ TesteApp/TesteApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace TesteApp
+{
+    internal class Program
+    {
+		public class Model
+		{
+			static List<(string description, string type, int max_size)> GetData()
+			{
+				return new List<(string, string, int)>
+				{
+					("name", "string", 20),
+					("password", "string", 20),
+					("email", "string", 20),
+					("phone", "number", 20)
+				};
+			}
+
+			public string Register(string name, string password, string email, int phone, string address)
+			{
+
+			}
+
+			public void teste()
+			{
+				var i = Register();
+				foreach (var n in i)
+					foreach (var s in n)
+						Console.WriteLine(n);
+			}
+		}
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+			Model i = new Model();
+			i.teste();
+        }
+    }
+}
