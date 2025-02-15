020d6204163dae19d1594b45b816e60dbc07b01e
HapyLen <leandroaurelio98@gmail.com>
2025-02-15 15:29:36 +0000

    added login model in lib

---
+++ Library/Library/Login/LoginService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libDeTeste
+{
+    public class LoginService
+    {
+        private readonly ILoginRepository _loginRepository;
+
+        public LoginService(ILoginRepository loginRepository)  
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public bool ValidarLogin(string email, string senha)
+        {
+            return _loginRepository.ValidarLogin(email, senha);
+        }
+
+        public LoginModel ObterUsuario(string email)
+        {
+            return _loginRepository.ObterUsuarioPorEmail(email);
+        }
+    }
+}
