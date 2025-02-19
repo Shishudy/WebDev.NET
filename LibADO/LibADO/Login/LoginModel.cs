dd858b89041172cba7b508e692d7695271c92c7f
HapyLen <leandroaurelio98@gmail.com>
2025-02-19 11:02:28 +0000

    ADO lib added + Login method

---
+++ LibADO/LibADO/Login/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibADO.Login;
+
+namespace LibADO.Login
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string UserRole { get; set; }
+    }
+}
