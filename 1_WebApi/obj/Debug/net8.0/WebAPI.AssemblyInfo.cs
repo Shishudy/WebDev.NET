bb8988b7a213227464fb8c44d3334dc674cfbb88
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-02-26 22:08:18 +0000

    Changes to HTML and implemented naming conventions

--- 1_WebApi/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ 1_WebApi/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096d7dd3e8064f91583ba1eee4343e551fa07121")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f1be37175cedb924c3f3cc12453e5088a119f5")]
+>>>>>>> b7bcc23 (Changes to HTML and implemented naming conventions)
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
