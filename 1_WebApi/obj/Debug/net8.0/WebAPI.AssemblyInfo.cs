454ae4ad3378d2d9b3e02bf4001caaec7af64c87
Rafael Carvalho <rafael.carvalh2012@gmail.com>
2025-02-27 12:55:14 +0000

    method dropdown done, working on form

--- 1_WebApi/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ 1_WebApi/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f1be37175cedb924c3f3cc12453e5088a119f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bcc23d8afe1c792f61292f66d4e6b17316eeab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
