9115f441232bcc4d224f87ca93a08635e536d362
HapyLen <leandroaurelio98@gmail.com>
2025-03-05 15:43:32 +0000

    Added exceptions UserMPA

--- 4_MPA/UserMPA/UserMPA/Pages/Requisitar.cshtml.cs
+++ 4_MPA/UserMPA/UserMPA/Pages/Requisitar.cshtml.cs
@@ -12,10 +12,12 @@ namespace UserMPA.Pages
 
         public List<Dictionary<string, object>> ObrasNosNucleos { get; set; } = new();
         public int PkLeitor { get; set; }
+
         public RequisitarModel(string connectionString)
         {
             _connectionString = connectionString;
         }
+
         public IActionResult OnGet(int pk_obra)
         {
             int? idUsuario = HttpContext.Session.GetInt32("PkLeitor");
@@ -26,18 +28,28 @@ namespace UserMPA.Pages
             }
 
             PkLeitor = idUsuario.Value;
+
             ObrasNosNucleos = Method.GetBookDetails(_connectionString, pk_obra);
 
             if (ObrasNosNucleos == null || ObrasNosNucleos.Count == 0)
             {
-                return NotFound();
+                TempData["ErrorMessage"] = "Esta obra não está disponível para requisição no momento.";
+                return RedirectToPage("/Search");
             }
+
             return Page();
         }
+
         public IActionResult OnPost(int pk_obra, int pk_leitor, int pk_nucleo)
         {
             try
             {
+                var detalhesObra = Method.GetBookDetails(_connectionString, pk_obra);
+                if (detalhesObra == null || detalhesObra.Count == 0)
+                {
+                    TempData["ErrorMessage"] = "Esta obra não pode ser requisitada, pois não está associada a um núcleo.";
+                    return RedirectToPage("/Search");
+                }
                 Method.ProcessRequisition(_connectionString, pk_leitor, pk_obra, pk_nucleo);
                 TempData["SuccessMessage"] = "Requisição realizada com sucesso!";
 
@@ -54,7 +66,7 @@ namespace UserMPA.Pages
                     TempData["ErrorMessage"] = $"Erro ao requisitar livro: {ex.Message}";
                 }
 
-                return Page();
+                return RedirectToPage("/Search");
             }
         }
     }
